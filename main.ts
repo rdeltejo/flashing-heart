4c0e4ec6acd607536080787285f1597724d6de17
rdeltejo <118136392+rdeltejo@users.noreply.github.com>
2022-11-13 17:11:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(100)
+})
